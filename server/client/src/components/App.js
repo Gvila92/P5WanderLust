76c63a0e9216c6888c51cd28c0daf2c27022ff83
Gvila92 <145710383+Gvila92@users.noreply.github.com>
2024-01-16 14:33:32 -0500

    Add files via upload

---
+++ server/client/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Login from './Login';
+import Signup from './Signup';
+import CreateBlog from './CreateBlog';
+import MyBlogs from './MyBlogs';
+import { AuthProvider } from '../contexts/AuthContext'; 
+
+function App() {
+  return (
+    <Router>
+      <AuthProvider> 
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/CreateBlog" element={<CreateBlog />} />
+          <Route path="/MyBlogs" element={<MyBlogs />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
+  );
+}
+
+export default App;
